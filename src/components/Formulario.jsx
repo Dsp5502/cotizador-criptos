ef0b8d20a359e3b08807fa97b09dcb2126ba7cae
David Puerto Guerrero <dsp5502@gmail.com>
2022-05-31 07:56:02 -0500

    Consultando una API

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect } from 'react';
 import styled from '@emotion/styled';
 import useSelectMonedas from '../hooks/useSelectMonedas';
 import { monedas } from '../data/monedas';
@@ -24,6 +24,18 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
   const [moneda, SelectMonedas] = useSelectMonedas('Elige tu moneda', monedas);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url =
+        'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
+      const resp = await fetch(url);
+      const { Data } = await resp.json();
+      console.log(Data);
+      return Data;
+    };
+    consultarAPI();
+  }, []);
 
   return (
     <form>
